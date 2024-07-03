d5cb9c217cab4c55d34dc501c5469fcdc338a974
Isaiah O A <damanagerltd@gmail.com>
2024-07-04 00:02:54 +0100

    updated to fix bug
    getting error 500x, notice bug on the code

--- modules/gateways/paystack.php
+++ modules/gateways/paystack.php
@@ -1,18 +1,17 @@
 <?php
 /**
- * ****************************************************************** **\
+ * ****************************************************************** **
  *                                                                      *
  *   Paystack Payment Gateway                                           *
  *   Version: 2.3.0                                                     *
- *   Build Date: April 25, 2024                                            *
+ *   Build Date: April 25, 2024                                         *
  *                                                                      *
  * **********************************************************************
  *                                                                      *
  *   Email: support@paystack.com                                        *
  *   Website: https://www.paystack.com                                  *
  *                                                                      *
-\
-************************************************************************/
+ ************************************************************************/
 
 if (!defined("WHMCS")) {
     die("<!-- Silence. SHHHHH!!!! -->");
@@ -27,7 +26,6 @@ function paystack_config()
 {
     $isSSL = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443);
     $callbackUrl = 'http' . ($isSSL ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] .
-        //substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], '/')) . '/'
         substr(str_replace('/admin/', '/', $_SERVER['REQUEST_URI']), 0, strrpos($_SERVER['REQUEST_URI'], '/')) .
         '/modules/gateways/callback/paystack.php';
 
@@ -40,7 +38,7 @@ function paystack_config()
             'FriendlyName' => 'Webhook URL',
             'Type' => 'yesno',
             'Description' => 'Copy and paste this URL on your Webhook URL settings <code>' . $callbackUrl . '</code>',
-            'Default' => "'" . $callbackUrl . "'",
+            'Default' => $callbackUrl,
         ),
         'gatewayLogs' => array(
             'FriendlyName' => 'Gateway logs',
@@ -67,7 +65,7 @@ function paystack_config()
             'Default' => 'pk_live_xxx'
         ),
         'testSecretKey' => array(
-            'FriendlyName' => 'Test Secrect Key',
+            'FriendlyName' => 'Test Secret Key',
             'Type' => 'text',
             'Size' => '32',
             'Default' => 'sk_test_xxx'
@@ -93,9 +91,7 @@ function paystack_link($params)
     // Client
     $email = $params['clientdetails']['email'];
     $phone = $params['clientdetails']['phonenumber'];
-    $params['langpaynow']
-        = array_key_exists('langpaynow', $params) ?
-        $params['langpaynow'] : 'Pay with Paystack';
+    $params['langpaynow'] = $params['langpaynow'] ?? 'Pay with Paystack';
 
     // Config Options
     if ($params['testMode'] == 'on') {
@@ -107,18 +103,17 @@ function paystack_link($params)
     }
 
     // check if there is an id in the GET meaning the invoice was loaded directly
-    $paynowload = (!array_key_exists('id', $_GET));
+    $paynowload = !isset($_GET['id']);
 
     // Invoice
     $invoiceId = $params['invoiceid'];
     $amountinkobo = round(floatval($params['amount']) * 100);
     $currency = $params['currency'];
-    ///Transaction_reference
+    // Transaction_reference
     $txnref = $invoiceId . '_' . time();
 
-
     if (!in_array(strtoupper($currency), ['NGN', 'USD', 'GHS', 'ZAR', 'EGP', 'XOF', 'KES', 'RWF'])) {
-        return ("<b style='color:red;margin:2px;padding:2px;border:1px dotted;display: block;border-radius: 10px;font-size: 13px;'>Sorry, this version of the Paystack WHMCS plugin only accepts NGN, USD, GHS, ZAR, EGP, XOF, KES, and RWF payments. <i>$currency</i> not yet supported.</b>");
+        return "<b style='color:red;margin:2px;padding:2px;border:1px dotted;display: block;border-radius: 10px;font-size: 13px;'>Sorry, this payment gateway only accepts NGN, USD, GHS, ZAR, EGP, XOF, KES, and RWF payments. <i>$currency</i> not yet supported.</b>";
     }
 
     $isSSL = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443);
@@ -166,9 +161,9 @@ function paystack_link($params)
         ' src="https://cdn-assets-cloud.frontify.com/s3/frontify-cloud-files-us/eyJwYXRoIjoiZnJvbnRpZnlcL2FjY291bnRzXC8yYVwvMTQxNzczXC9wcm9qZWN0c1wvMTc4NjE0XC9hc3NldHNcLzdmXC8yNzQ2ODcxXC83NDY5OGViODMzMzhlMWJiNjVhMDk4MTYwNjkzY2FlOC0xNTQwMDM5NjA0LnBuZyJ9:cloud:YYqwtVK3Tb8KMGeFiXCl_w9flKcsEY9D022GMOK9oFc"/>\';
 
                     $(\'.payment-btn-container\').append(toAppend);
-                    if($(\'.payment-btn-container\').length===0){
+                    if ($(\'.payment-btn-container\').length === 0) {
                         $(\'select[name="gateway"]\').after(toAppend);
-                   }
+                    }
                 }
             });
         </script>
@@ -176,32 +171,32 @@ function paystack_link($params)
     <div class="hidden" style="display:none"><iframe name="hiddenIFrame"></iframe></div>
     <script>
         var button_created = false;
-        var paystackPop  = new PaystackPop()
+        var paystackPop  = new PaystackPop();
 
-        function payWithPaystack(){
+        function payWithPaystack() {
             paystackPop.checkout({
                 key: \'' . addslashes(trim($publicKey)) . '\',
                 email: \'' . addslashes(trim($email)) . '\',
                 phone: \'' . addslashes(trim($phone)) . '\',
                 amount: ' . $amountinkobo . ',
                 currency: \'' . addslashes(trim($currency)) . '\',
-                ref:\'' . $txnref . '\',
-                metadata:{
-                    "custom_fields":[
-                      {
-                        "display_name":"Plugin",
-                        "variable_name":"plugin",
-                        "value":"whmcs"
-                      }
+                ref: \'' . $txnref . '\',
+                metadata: {
+                    "custom_fields": [
+                        {
+                            "display_name": "Plugin",
+                            "variable_name": "plugin",
+                            "value": "whmcs"
+                        }
                     ]
-                  },
-                onSuccess: function(response){
+                },
+                onSuccess: function(response) {
                     $(\'div.alert.alert-info.text-center\').hide();
                     $(\'.payment-btn-container2\').hide();
                     
                     window.location.href = \'' . addslashes($callbackUrl) . '&trxref=\' + response.trxref;
                 },
-                onCancel: function(){
+                onCancel: function() {
                 }
             });
             $(\'img[alt="Loading"]\').hide();
@@ -209,19 +204,20 @@ function paystack_link($params)
             create_button();
         }
 
-       function create_button(){
-        if(!button_created){
-            button_created = true;
-            $(\'.payment-btn-container2\').append(\'<button type="button"' .
+        function create_button() {
+            if (!button_created) {
+                button_created = true;
+                $(\'.payment-btn-container2\').append(\'<button type="button"' .
         ' onClick="window.location.reload()"' .
         ' style="padding: 10px 25px; margin: 10px;border-radius: 5px;background: #021C32; color:#fff">' .
         addslashes($params['langpaynow']) . '</button>' .
         '<img style="width: 150px; display: block; margin: 0 auto;"' .
         ' src="https://cdn-assets-cloud.frontify.com/s3/frontify-cloud-files-us/eyJwYXRoIjoiZnJvbnRpZnlcL2FjY291bnRzXC8yYVwvMTQxNzczXC9wcm9qZWN0c1wvMTc4NjE0XC9hc3NldHNcLzdmXC8yNzQ2ODcxXC83NDY5OGViODMzMzhlMWJiNjVhMDk4MTYwNjkzY2FlOC0xNTQwMDM5NjA0LnBuZyJ9:cloud:YYqwtVK3Tb8KMGeFiXCl_w9flKcsEY9D022GMOK9oFc"/>\');
+            }
         }
-       }     
-       ' . ($paynowload ? 'setTimeout("payWithPaystack()", 5100);' : '') . '
+        ' . ($paynowload ? 'setTimeout("payWithPaystack()", 5100);' : '') . '
     </script>';
 
     return $code;
 }
+?>
